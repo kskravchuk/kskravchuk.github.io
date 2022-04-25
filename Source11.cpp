b2ae9a6f6f083b17df996dc69531870b25b771a7
kskravchuk <100480766+kskravchuk@users.noreply.github.com>
2022-04-25 15:12:57 +0300

    Add files via upload

---
+++ Source11.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+long counter, x; //
+void fun1()
+{
+	printf(" x(fun1): ");
+	scanf_s("%ld", &x);
+}
---
+++ Source22.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+extern long counter, x;
+void fun2()
+{
+	printf("¬ведите counter(fun2): ");
+	scanf_s("%ld", &counter);
+}
